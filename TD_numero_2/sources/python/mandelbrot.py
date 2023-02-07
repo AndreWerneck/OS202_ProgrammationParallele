26f05adefc2b88250e83e977294d83e473bb8057
AndreWerneck <40474628+AndreWerneck@users.noreply.github.com>
2023-02-07 02:25:32 +0100

    solution of question 2.3.1

--- TD_numero_2/sources/python/mandelbrot.py
+++ TD_numero_2/sources/python/mandelbrot.py
@@ -5,6 +5,14 @@ from PIL import Image
 from math import log
 from time import time
 import matplotlib.cm
+from mpi4py import MPI
+
+globCom = MPI.COMM_WORLD.Dup()
+nbp = globCom.size
+rank = globCom.rank
+# name = MPI.Get_processor_name()
+# print(f"Je suis le processus {rank} sur {nbp} processus")
+# print(f"Je m'execute sur l'ordinateur {name}")
 
 @dataclass
 class MandelbrotSet:
@@ -14,11 +22,11 @@ class MandelbrotSet:
     def __contains__(self, c: complex) -> bool:
         return self.stability(c) == 1
 
-    def convergence(self, c:complex, smooth=False, clamp=True):
+    def convergence(self, c:complex, smooth=False, clamp=True) -> float:
         value = self.count_iterations(c,smooth)/self.max_iterations
         return max(0.0, min(value, 1.0)) if clamp else value
 
-    def count_iterations(self, c: complex,  smooth=False):
+    def count_iterations(self, c: complex,  smooth=False) -> int | float :
         z    : complex
         iter : int
 
@@ -45,25 +53,51 @@ class MandelbrotSet:
                 return iter
         return self.max_iterations
 
+
+
 # On peut changer les paramètres des deux prochaines lignes
-mandelbrot_set = MandelbrotSet(max_iterations=50,escape_radius=10)
+mandelbrot_set = MandelbrotSet(max_iterations=65,escape_radius=10)
 width, height = 1024, 1024
 
 scaleX = 3./width
 scaleY = 2.25/height
 convergence = np.empty((width,height),dtype=np.double)
+scat_data = []
+
+if(rank==0):
+    for i in range(0,nbp):
+        scat_data.append(convergence[:,i*int(height/nbp):(i+1)*int(height/nbp)])
+
+scat_data = globCom.scatter(scat_data,root=0)
+
 # Calcul de l'ensemble de mandelbrot :
 deb = time()
-for y in range(height):
+for y in range(rank*int(height/nbp),(rank+1)*int(height/nbp)): #first itering at the columns -> so, itering line per line
     for x in range(width):
         c = complex(-2. + scaleX*x, -1.125 + scaleY * y)
-        convergence[x,y] = mandelbrot_set.convergence(c,smooth=True)
+        scat_data[x,y - (rank*int(height/nbp))] = mandelbrot_set.convergence(c,smooth=True)
+
 fin = time()
 print(f"Temps du calcul de l'ensemble de Mandelbrot : {fin-deb}")
 
+image = Image.fromarray(np.uint8(matplotlib.cm.plasma(scat_data.T)*255))
+# image.show()
+
+
+
 # Constitution de l'image résultante :
 deb=time()
-image = Image.fromarray(np.uint8(matplotlib.cm.plasma(convergence.T)*255))
-fin = time()
-print(f"Temps de constitution de l'image : {fin-deb}")
-image.show()
+gat_data = globCom.gather(scat_data,root=0)
+
+if(rank == 0):
+    img = []
+    for i in range(0, nbp):
+        img = img + gat_data[i].T.tolist()
+    
+    img = np.array(img)    
+    image = Image.fromarray(np.uint8(matplotlib.cm.plasma(img)*255))
+
+    fin = time()
+    print(f"Temps de constitution de l'image : {fin-deb}")
+    # image.show()
+    
